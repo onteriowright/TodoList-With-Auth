86fc912302e3700cac73bc96d6d2e0f4a00ef940
onteriowright <onteriowright@yahoo.com>
2020-04-22 14:07:24 -0500

    first commit

---
+++ ToDoList/Models/ToDoItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public int ToDoStatusId { get; set; }
+        [Required]
+        public ToDoStatus ToDoStatus { get; set; }
+        public int ApplicationUserId { get; set; }
+        [Required]
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
